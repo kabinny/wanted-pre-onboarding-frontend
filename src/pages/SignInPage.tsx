f5d18c228b8a5e15c97911157111b52e9b129ff2
kabinny <kabinny@gmail.com>
2023-02-11 00:26:30 +0900

    feat: 토큰 있으면 todo로 리다이렉트

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import Header from "../components/Header/Header"
 import Container from "../components/Layout/Container"
 import SignIn from "../components/SignIn/SignIn"
 
 function SignInPage() {
+  const isLoggedIn = localStorage.getItem("access_token")
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/todo")
+    }
+  }, [])
+
   return (
     <Container>
       <Header />
