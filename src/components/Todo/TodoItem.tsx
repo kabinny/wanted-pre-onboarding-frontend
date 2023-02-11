93b58761ad236c54f46e6311fcc5029028982fb6
kabinny <kabinny@gmail.com>
2023-02-12 01:03:43 +0900

    feat: 수정 삭제 버튼 추가

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -27,6 +27,12 @@ function TodoItem({ id, todo, isCompleted, userId, getTodoList }: Props) {
         <input type="checkbox" onChange={onCheck} defaultChecked={isCompleted} />
         <span>{todo}</span>
       </label>
+      <button type="button" data-testid="modify-button">
+        수정
+      </button>
+      <button type="button" data-testid="delete-button">
+        삭제
+      </button>
     </li>
   )
 }
